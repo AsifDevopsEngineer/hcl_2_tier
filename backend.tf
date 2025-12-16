c54283d152b6c901b6b89c5935ba6310b8104e2d
AsifDevopsEngineer <120320386+AsifDevopsEngineer@users.noreply.github.com>
2025-12-16 16:08:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 
 terraform {
-  required_version = ">= 1.3.0"
+
 
   backend "s3" {
     bucket         = "my-demo-bucket-asif"          # Your S3 bucket name
@@ -9,16 +9,7 @@ terraform {
     dynamodb_table = "my-demo-table-asif"           # DynamoDB table for state locking
     encrypt        = true                           # Enable server-side encryption
   }
-
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
 }
 
-provider "aws" {
-  region = "ap-south-1"
-}
+
+
