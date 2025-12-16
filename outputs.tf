320116c2e53f3215f7248688efafcd47a27c46cb
AsifDevopsEngineer <120320386+AsifDevopsEngineer@users.noreply.github.com>
2025-12-16 13:39:02 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,25 @@
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "public_route_table_id" {
+  description = "Public route table ID"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_ids" {
+  description = "Private route table IDs"
+  value       = aws_route_table.private[*].id
+}
