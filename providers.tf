445ecfd12b7f958ae6f00ce07e7f129733df6e09
AsifDevopsEngineer <120320386+AsifDevopsEngineer@users.noreply.github.com>
2025-12-17 05:32:09 +0530

    Update Terraform required version and AWS region variable

--- providers.tf
+++ providers.tf
@@ -1,6 +1,5 @@
-
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
@@ -11,5 +10,6 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
+
