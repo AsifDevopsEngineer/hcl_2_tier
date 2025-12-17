87e3c94f11029466f11d1b8c140b669243ccc2a5
AsifDevopsEngineer <120320386+AsifDevopsEngineer@users.noreply.github.com>
2025-12-17 05:35:29 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,50 +1,7 @@
-
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
-}
-
-output "public_subnet_ids" {
-  description = "IDs of public subnets"
-  value       = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  description = "IDs of private subnets"
-  value       = aws_subnet.private[*].id
-}
-
-output "public_route_table_id" {
-  description = "Public route table ID"
-  value       = aws_route_table.public.id
-}
-
-output "private_route_table_ids" {
-  description = "Private route table IDs"
-  value       = aws_route_table.private[*].id
-}
-
 output "alb_dns_name" {
-  description = "DNS name of the ALB"
-  value       = aws_lb.alb.dns_name
+  value = aws_lb.alb.dns_name
 }
 
-output "alb_zone_id" {
-  description = "Route53 zone ID for ALB"
-  value       = aws_lb.alb.zone_id
-}
-
-output "alb_security_group_id" {
-  description = "Security Group ID for ALB"
-  value       = aws_security_group.alb_sg.id
-}
-
-output "web_security_group_id" {
-  description = "Security Group ID for web/app servers"
-  value       = aws_security_group.web_sg.id
-}
-
-output "web_target_group_arn" {
-  description = "Target group ARN for web tier"
-  value       = aws_lb_target_group.web_tg.arn
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.endpoint
 }
